773d1a4f73e4d2afa3bf5ba8bdef5de2aba60c05
Shawn McLaughlin <shawnmcdev@gmail.com>
2018-06-19 19:41:04 -0400

    guildOnly, cooldowns

--- commands/guild-only-test.js
+++ commands/guild-only-test.js
@@ -1,14 +1,10 @@
 module.exports = {
-    name: 'guildOnlyTest',
+    name: 'guild-only-test',
     description: 'Testing Guild Only functionality.',
     args: false,
     usage: '',
     guildOnly: true,
     execute(message, args){
-        if(args[0] === 'foo'){
-            return message.channel.send('bar')
-        }
-
-        message.channel.send(`Arguments: ${args}\nArguments length: ${args.length}`)
+        return message.channel.send(`This is fine.`)
     }
 }
--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const fs = require('fs')
-const discord = require('discord.js')
+const Discord = require('discord.js')
 
-const {prefix, token} = require('./config.json')
+const { prefix, token } = require('./config.json')
 
-const client = new discord.Client()
+const client = new Discord.Client()
 
 // Dynamically load commands
 
-client.commands = new discord.Collection()
+client.commands = new Discord.Collection()
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
 
-for(const file of commandFiles){
+for (const file of commandFiles) {
     const command = require(`./commands/${file}`)
     client.commands.set(command.name, command)
 }
 
+const cooldowns = new Discord.Collection()
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}..`)
@@ -24,7 +25,7 @@ client.on('ready', () => {
 client.on('message', message => {
     //$devonly
     console.log("Read Message: " + message.content)
-    
+
     if (message.content === 'ping') {
         message.reply('pong')
     }
@@ -36,24 +37,44 @@ client.on('message', message => {
     //$devonly
     console.log("Received command: ", commandName, args)
 
-    if(!client.commands.has(commandName)) return
+    if (!client.commands.has(commandName)) return
 
     const command = client.commands.get(commandName)
 
     // Check that context is correct (guild channel vs DM)
-    if(command.guildOnly && message.channel.type !== 'text'){
+    if (command.guildOnly && message.channel.type !== 'text') {
         return message.reply('This command cannot be used in DMs.')
     }
     // Check that if the command requires arguments that arguments are present
-    if(command.args&&!args.length){
+    if (command.args && !args.length) {
         return message.channel.send(`You didn't provide any arguments, ${message.author}!` +
-         (command.usage ? `\nProper usage: \`${prefix}${command.name} ${command.usage}\`` : ""))
+            (command.usage ? `\nProper usage: \`${prefix}${command.name} ${command.usage}\`` : ""))
+    }
+    // Verify cooldowns
+    if (!cooldowns.has(command.name)) cooldowns.set(command.name, new Discord.Collection())
+    const now = Date.now()
+    const timestamps = cooldowns.get(command.name)
+    const cooldownAmount = (command.cooldown || 3) * 1000
+    if (!timestamps.has(message.author.id)) {
+        timestamps.set(message.author.id, now)
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
+    }
+    else {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000
+            return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${commandName}\` command.`)
+        }
+
+        timestamps.set(message.author.id, now)
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
     }
     // Execute the command
-    try{
-       command.execute(message, args)
+    try {
+        command.execute(message, args)
     }
-    catch(error){
+    catch (error) {
         console.error(error)
         message.reply("God damnit Todd, the bot is fucked. Pick up your shit! (Error processing command)")
     }

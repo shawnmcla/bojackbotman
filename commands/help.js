327495417404e1facb9884f6f1dc5d094ad40393
Shawn McLaughlin <shawnmcdev@gmail.com>
2018-06-20 17:56:47 -0400

    added unlisted commands, removed empty files

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,9 @@ module.exports = {
 
         if (!args.length) {
             data.push('Here is a list of all my commands:')
-            data.push(commands.map(command => command.name + (command.roles && command.roles.length ? "(!)" : '')).join(', '))
+            data.push(commands
+                .filter(command => !command.unlisted)
+                .map(command => command.name + (command.roles && command.roles.length ? "(!)" : '')).join(', '))
             data.push(`\nCommands denoted with a \`!\` require elevated privileges.\nYou can type \`${prefix}help [command name]\` to get information on a specific command.`)
 
             return message.author.send(data, { split: true })
@@ -35,7 +37,7 @@ module.exports = {
         if (command.aliasses && command.aliases.length) data.push(`**Aliases:** ${command.aliases.join(', ')}`)
         if (command.description) data.push(`**Description:** ${command.description}`)
         if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`)
-        if(command.roles && command.roles.length) data.push(`**Roles:** ${command.roles.join(', ')}`)
+        if (command.roles && command.roles.length) data.push(`**Roles:** ${command.roles.join(', ')}`)
 
         data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`)
 
--- commands/ping.js
+++ commands/ping.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'Ping!',
     cooldown: 5,
     aliases: ['pang'],
+    unlisted: true,
     execute(message, args){
         message.channel.send('Pong.')
     }
